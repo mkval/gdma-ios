421f923239815e96fe5a4c5091f4188747efa85b
Mark Valles <mkvalles@gmail.com>
2024-07-03 22:59:10 +0800

    Initial Commit

---
+++ Guidomia/GuidomiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidomiaApp.swift
+//  Guidomia
+//
+//  Created by Mark Valles on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GuidomiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
